c021d37b7936d6d5ec49791321af914f1efdc580
Nikola Gedović <nikola.gedovic@gmail.com>
2025-08-06 17:19:50 +0200

    adding styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <StyledApp>
+      <H1>The Wild Oasis</H1>
+      <Button>Check in</Button>
+    </StyledApp>
+  );
 }
 
 export default App;
