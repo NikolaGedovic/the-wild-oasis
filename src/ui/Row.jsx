aca2b18c7d09d76b0945dc8469077c0676d69c5f
Nikola Gedović <nikola.gedovic@gmail.com>
2025-08-09 09:05:46 +0200

    setting up pages and routes

--- src/ui/Row.jsx
+++ src/ui/Row.jsx
@@ -1,26 +1,21 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const Row = styled.div`
   display: flex;
 
-  ${(props) =>
-    props.type === "horizontal" &&
+  ${({ $orientation = "vertical" }) =>
+    $orientation === "horizontal" &&
     css`
       justify-content: space-between;
       align-items: center;
     `}
 
-  ${(props) =>
-    props.type === "vertical" &&
+  ${({ $orientation = "vertical" }) =>
+    $orientation === "vertical" &&
     css`
       flex-direction: column;
       gap: 1.6rem;
     `}
 `;
-
-Row.defaultProps = {
-  variation: "primary",
-  size: "medium",
-};
 
 export default Row;
