ba36cdeaca37af6c66d3f5b360388e9031573eec
Nikola Gedović <nikola.gedovic@gmail.com>
2025-08-10 10:54:21 +0200

    fetching cabin data

--- src/ui/Row.jsx
+++ src/ui/Row.jsx
@@ -3,15 +3,15 @@ import styled, { css } from "styled-components";
 const Row = styled.div`
   display: flex;
 
-  ${({ $orientation = "vertical" }) =>
-    $orientation === "horizontal" &&
+  ${({ type = "vertical" }) =>
+    type === "horizontal" &&
     css`
       justify-content: space-between;
       align-items: center;
     `}
 
-  ${({ $orientation = "vertical" }) =>
-    $orientation === "vertical" &&
+  ${({ type = "vertical" }) =>
+    type === "vertical" &&
     css`
       flex-direction: column;
       gap: 1.6rem;
