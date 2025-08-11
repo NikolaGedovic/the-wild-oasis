b17a9aebe346a3dda92a0347407fd4b51e9d06ae
Nikola Gedović <nikola.gedovic@gmail.com>
2025-08-11 18:30:23 +0200

    Handling form error, and validating inputs in the form

---
+++ src/ui/FormRow.jsx
@@ -0,0 +1,66 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+const StyledFormRow = styled.div`
+  display: grid;
+  align-items: center;
+  grid-template-columns: 24rem 1fr 1.2fr;
+  gap: 2.4rem;
+
+  padding: 1.2rem 0;
+
+  &:first-child {
+    padding-top: 0;
+  }
+
+  &:last-child {
+    padding-bottom: 0;
+  }
+
+  &:not(:last-child) {
+    border-bottom: 1px solid var(--color-grey-100);
+  }
+
+  &:has(button) {
+    display: flex;
+    justify-content: flex-end;
+    gap: 1.2rem;
+  }
+`;
+
+const Label = styled.label`
+  font-weight: 500;
+`;
+
+const Error = styled.span`
+  font-size: 1.4rem;
+  color: var(--color-red-700);
+`;
+
+// Global styles for inputs and textarea inside FormRow to take full width
+const InputsFullWidth = createGlobalStyle`
+  input,
+  textarea {
+    width: 100%;
+    box-sizing: border-box;
+  }
+
+  textarea {
+    resize: vertical;
+    min-height: 6rem;
+  }
+`;
+
+function FormRow({ label, error, children }) {
+  return (
+    <>
+      <InputsFullWidth />
+      <StyledFormRow>
+        {label && <Label htmlFor={children.props.id}>{label}</Label>}
+        <div style={{ width: "100%" }}>{children}</div>
+        {error && <Error>{error}</Error>}
+      </StyledFormRow>
+    </>
+  );
+}
+
+export default FormRow;
