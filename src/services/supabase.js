0bdb0d691295cd4b460cd2188949fae18e0c4cd3
Nikola Gedović <nikola.gedovic@gmail.com>
2025-08-12 17:51:55 +0200

    uploading images to supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://tdnomrgyheypscctnijn.supabase.co";
+export const supabaseUrl = "https://tdnomrgyheypscctnijn.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRkbm9tcmd5aGV5cHNjY3RuaWpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ3NDQxMjksImV4cCI6MjA3MDMyMDEyOX0.Q-I7mTRYSQrhMYKjio4rSxTPsAc-Q0gEKTNuXRqgGI8";
 
